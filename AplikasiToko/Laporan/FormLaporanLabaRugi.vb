d73c4a8b38b2e463904eadabf97fbd26e89fd6c7
Ceater <johan079a19@yahoo.co.id>
2017-12-03 10:27:43 +0700

    Penambahan Form Laba Rugi, Masih Belum Selesai.

---
+++ AplikasiToko/Laporan/FormLaporanLabaRugi.vb
@@ -0,0 +1,72 @@
+﻿Imports System.Data.Sql
+Imports System.Data.SqlClient
+
+Public Class FormLaporanLabaRugi
+    Public Sub New()
+        InitializeComponent()
+
+    End Sub
+
+    Function getPersediaanAwal()
+        Dim hsl As Integer = 0
+        constring.Open()
+        Try
+            cmd = New SqlCommand("select PersediaanAwal from TbLabaRugi", constring)
+            hsl = cmd.ExecuteScalar
+        Catch ex As Exception
+
+        End Try
+        constring.Close()
+        Return hsl
+    End Function
+
+    Function getPersediaanAkhir()
+        Dim hsl As Integer = 0
+        constring.Open()
+        Try
+            cmd = New SqlCommand("select Sum(Stok * HargaSales) From TbBarang", constring)
+            hsl = cmd.ExecuteScalar
+        Catch ex As Exception
+
+        End Try
+        constring.Close()
+        Return hsl
+    End Function
+
+    Function getPenjualan()
+        Dim hsl As Integer = 0
+        constring.Open()
+        Try
+            cmd = New SqlCommand("select sum(grandtotal) from HJual", constring)
+            hsl = cmd.ExecuteScalar
+        Catch ex As Exception
+
+        End Try
+        constring.Close()
+        Return hsl
+    End Function
+
+    Function getRetur()
+        Dim hsl As Integer = 0
+        constring.Open()
+        Try
+
+        Catch ex As Exception
+
+        End Try
+        constring.Close()
+        Return hsl
+    End Function
+
+    Function getPembelian()
+        Dim hsl As Integer = 0
+        constring.Open()
+        Try
+
+        Catch ex As Exception
+
+        End Try
+        constring.Close()
+        Return hsl
+    End Function
+End Class
