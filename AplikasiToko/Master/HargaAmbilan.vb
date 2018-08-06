f3e12d5e212a8dd17e385b2e1573348b55a2f257
Ceater <johan079a19@yahoo.co.id>
2018-08-06 08:09:08 +0700

    -

---
+++ AplikasiToko/Master/HargaAmbilan.vb
@@ -0,0 +1,11 @@
+﻿Public Class HargaAmbilan
+
+    Private Sub HargaAmbilan_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DataGridView1.DataSource = ModuleHargaAmbilan.getData("").Tables("DataAmbilan")
+
+    End Sub
+
+    Private Sub TextBox1_KeyUp(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyUp
+        DataGridView1.DataSource = ModuleHargaAmbilan.getData(TextBox1.Text).Tables("DataAmbilan")
+    End Sub
+End Class
---
+++ AplikasiToko/Module/Master/ModuleHargaAmbilan.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Data.Sql
+Imports System.Data.SqlClient
+
+Module ModuleHargaAmbilan
+    Function getData(keyword As String) As DataSet
+        keyword = "%" & keyword & "%"
+        Dim result As New DataSet
+        Dim adpter As New SqlDataAdapter
+
+        constring.Open()
+        Try
+            adpter.SelectCommand = New SqlCommand("SELECT tb.KodeBarang, tb.NamaBarang, ISNULL(dp.HargaSatuan, 0) as HargaAmbilan FROM TbBarang tb LEFT JOIN DPembelian dp ON tb.KodeBarang=dp.IDBarang WHERE (tb.KodeBarang LIKE @a) OR (tb.NamaBarang LIKE @b)", constring)
+            adpter.SelectCommand.Parameters.Add("@a", SqlDbType.VarChar, 25, "%")
+            adpter.SelectCommand.Parameters.Add("@b", SqlDbType.VarChar, 25, "%")
+            adpter.Fill(result, "DataAmbilan")
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+        constring.Close()
+        Return result
+    End Function
+End Module
